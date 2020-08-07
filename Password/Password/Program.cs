fecbed5262fa7d36289b49a5e27e6a871f084ae1
RandomLite <fatnisi_a@hotmail.com>
2020-08-07 15:28:36 +0200

    Find-Num-Password
    Simple code made with C# Programming language which executes with Console Application to get the specified Num password.

---
+++ Password/Password/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pas pas = new Pas();
+            
+            pas.Pass();
+        }
+    }
+
+}
+
+public class Pas
+{
+ public void Pass()
+    {
+        // 1st -Random Password
+
+        Random random = new Random();
+      System.Int64 passi = 354623;
+        Int64 i = 1,psw=1,j=1;
+        here:
+       int ps = random.Next(100000, 1000000);
+        //Console.WriteLine(ps);
+        if (ps!=passi)
+        {
+           // ps++;
+            i++;
+            goto here;
+           
+        }
+        else
+        {
+            Console.WriteLine(" ------- Random search Password! ----------");
+            Console.WriteLine("\nAfter {0} combinations",i);
+                Console.WriteLine("Password is succesfully found: "+ps);           
+        }
+
+        
+        //Linear search Password
+
+       
+    me:
+      
+        if (psw != passi)
+        {
+            psw++;
+            j++;
+            goto me;
+
+        }
+        else
+        {
+            Console.WriteLine("\n\n ------- Linear search Password! ----------");
+            Console.WriteLine("\nAfter {0} combinations", j);
+            Console.WriteLine("Password is succesfully found: " + psw);
+
+        }
+    }
+}
